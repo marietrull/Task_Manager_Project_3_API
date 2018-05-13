f72c7c71a1e2db5bebfa9de29ae9a3d2774601df
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-05-13 17:14:26 -0500

    Created login route in user controller

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -4,6 +4,8 @@ class UserController < ApplicationController
 
 	# Routes
 
+	# Register Route
+
 	post '/register' do 
 
 		user = User.new
@@ -30,6 +32,48 @@ class UserController < ApplicationController
 		
 	end
 
+
+
+
+	# Login Route
+
+	post '/login' do
+
+		username = @payload[:username]
+
+		password = @payload[:password]
+
+		user = User.find_by :username => username
+
+
+
+		if user && user.authenticate(password)
+
+			session[:logged_in] = true
+
+			session[:username] = username
+
+			{
+				success: true,
+
+				username: session[:username],
+
+				message: "Logged in as #{session[:username]}."
+
+			}.to_json
+
+		else 
+
+			{
+				success: false,
+
+				message: "Invalid username or password."
+			}.to_json
+
+		end	
+
+		
+	end
 
 
 end	
