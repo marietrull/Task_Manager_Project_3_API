09bd471cf1846db96d4a7022de668ed7b988cce4
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-05-15 16:14:39 -0500

    created tabs for HW and Outcome controller; Styled Add Button

--- task-manager/src/HomeworkContainer/index.js
+++ task-manager/src/HomeworkContainer/index.js
@@ -178,20 +178,32 @@ class HomeworkContainer extends Component {
 	render () {
 		console.log(this.state);
 		return (
+			<div className="LogRegField">
 
 
-			<div id='homeworkContainer'>
-				<Homework assignments={this.state.assignments} openEdit={this.openEdit}showHWModal={this.showHWModal}/>
-				<button id='addButton' onClick={this.openAdd}> Add New Assignment </button>
+				<div className="MainTab">
+					<div className="LoginTab Tab" >Outcomes</div> 
 
-				<CreateHomeworkModal addAssignment={this.addAssignment} openEdit={this.openEdit} showAdd={this.state.showAdd}/>
+					<div className="RegisterTab Tab" >Homework</div>
+				</div>	
 
-				<ShowHWModal hwShowing={this.state.hwShowing} hwModalOpen={this.state.hwModalOpen} closeModal={this.closeModal}/>
 
-				<EditHomeworkModal showEdit={this.state.showEdit} editAssignment={this.editAssignment} removeAssignment={this.removeAssignment} editedAssignment={this.state.editedAssignment}/>
+
+				<div id='homeworkContainer'>
+					<Homework assignments={this.state.assignments} openEdit={this.openEdit}showHWModal={this.showHWModal}/>
+					<button id='addButton'  onClick={this.openAdd}> Add New Assignment </button>
+
+					<CreateHomeworkModal addAssignment={this.addAssignment} openEdit={this.openEdit} showAdd={this.state.showAdd}/>
+
+					<ShowHWModal hwShowing={this.state.hwShowing} hwModalOpen={this.state.hwModalOpen} closeModal={this.closeModal}/>
+
+					<EditHomeworkModal showEdit={this.state.showEdit} editAssignment={this.editAssignment} removeAssignment={this.removeAssignment} editedAssignment={this.state.editedAssignment}/>
+				</div>
+
+
+
 			</div>
-			
-			)
+		)
 
 	}
 }
--- task-manager/src/LoginRegister/index.js
+++ task-manager/src/LoginRegister/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.css';
 
 
-const LoginRegister = ({loginAndRegister, registering,changeRegistering}) => {
+const LoginRegister = ({loginAndRegister, registering, changeRegistering}) => {
 
 	return(
 	
